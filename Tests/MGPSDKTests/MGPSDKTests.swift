bda309529352158ecb7fc81c10a94380bb12bea0
Josh Monroe <josh_monroe@mastercard.com>
2017-09-12 15:19:28 -0500

    initial project setup

---
+++ Tests/MGPSDKTests/MGPSDKTests.swift
@@ -0,0 +1,13 @@
+import Foundation
+import XCTest
+import MGPSDK
+
+class MGPSDKTests: XCTestCase {
+    func testExample() {
+        
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
