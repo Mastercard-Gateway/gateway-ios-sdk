0968f3a3ec4adc56b25727b63e81708bea2cb6e0
Balazs Bereczky <balazs.bereczky@wecanconsulting.eu>
2023-10-04 16:08:37 +0200

    guess

--- Package.swift
+++ Package.swift
@@ -3,23 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "MPGSDK",
-    platforms: [
-        .iOS(.v12)
-    ],
     products: [
         .library(
             name: "MPGSDK",
             targets: ["MPGSDK"]
         )
     ],
-    dependencies: [],
     targets: [
         .target(
             name: "MPGSDK",
-            dependencies: [],
-            path: "MPGSDK"  // Replace with the actual path to your source files
-        )
-    ],
-    swiftLanguageVersions: [.v3, .v4, .v5]
+            dependencies: []
+        )    
+    ]
 )
-
