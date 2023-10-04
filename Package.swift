57dccd892ff75b29a94170b12f6a28cd5777dbf3
Balazs Bereczky <balazs.bereczky@wecanconsulting.eu>
2023-10-04 15:52:07 +0200

    guess

--- Package.swift
+++ Package.swift
@@ -1,18 +1,24 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(
-    name: "MPGSDK",
-    products: [
-        .library(
-            name: "MPGSDK",
-            targets: ["MPGSDK"]
-        ),
-    ],
-    targets: [
-        .target(
-            name: "MPGSDK",
-            dependencies: []
-        ),    
-    ]
-)
+import PackageDescription
+
+let pkg = Package(name: "MPGSDK")
+pkg.products = [
+    .library(name: "MPGSDK", targets: ["MPGSDK"]),
+]
+
+let mpg: Target = .target(name: "MPGSDK")
+mpg.path = "Sources"
+mpg.exclude = [
+    // List files or directories you want to exclude from the build process
+    // For example: "SomeFile.swift"
+]
+pkg.swiftLanguageVersions = [3, 4, 5]  // Assuming MPGSDK uses Swift 5
+pkg.targets = [
+    mpg,
+    // Add any test targets or other targets related to MPGSDK
+    // For example:
+    // .testTarget(name: "MPGSDKTests", dependencies: ["MPGSDK"], path: "Tests/MPGSDKTests"),
+]
+
